b074bb6e5dbe2de7c1eee58e051e5c36facde36a
harissheikh8775 <harisbadboi@gmail.com>
2024-10-27 19:53:57 +0530

    day_5

--- day_5/passGenerator.py
+++ day_5/passGenerator.py
@@ -1,35 +1,46 @@
+#Password Generator Project
 import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Convert inputs to integers
-letters_want = int(input("How many letters would you like in your password?\n"))
-symbols_want = int(input("How many symbols would you like?\n"))
-numbers_want = int(input("How many numbers would you like?\n"))
+#Eazy Level
+# password = ""
 
-letters = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~']
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
 
-password = []
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
 
-# Generate random letters
-for _ in range(letters_want):
-    password.append(random.choice(letters))
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
 
-# Generate random symbols
-for _ in range(symbols_want):
-    password.append(random.choice(symbols))
+# print(password)
 
-# Generate random numbers
-for _ in range(numbers_want):
-    password.append(random.choice(numbers))
+#Hard Level
+password_list = []
 
-# Shuffle the password list to randomize it
-random.shuffle(password)
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
 
-# Join list elements into a single string and print the result
-print("Generated Password: ", ''.join(password))
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
