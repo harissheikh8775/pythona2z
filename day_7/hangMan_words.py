fd851679e928a238162f88976ad739859dffc3aa
harissheikh8775 <harisbadboi@gmail.com>
2024-10-28 13:34:55 +0530

    hangMan

---
+++ day_7/hangMan_words.py
@@ -0,0 +1,5 @@
+word_list = ["aardvark", "baleia", "camelo", "doninha", "elefante",
+             "flamingo", "galinha", "hiena", "iguana", "jararaca",
+             "kiwi", "lince", "morsa", "narval", "ovelha", "papagaio",
+             "quati", "rinoceronte", "suricate", "tartaruga", "urso",
+             "vagalume", "wallaby", "ximango", "yak", "zebra"]
