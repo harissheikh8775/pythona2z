d12d5aaf8d3fbe17f31b03af563b5ed7c97a018e
harissheikh8775 <harisbadboi@gmail.com>
2024-10-29 18:58:48 +0530

    marksToGrade

---
+++ day_9/marksToGrade.py
@@ -0,0 +1,26 @@
+# This is the scoring criteria: 
+
+# - Scores 91 - 100: Grade = "Outstanding" 
+
+# - Scores 81 - 90: Grade = "Exceeds Expectations" 
+
+# - Scores 71 - 80: Grade = "Acceptable" 
+
+# - Scores 70 or lower: Grade = "Fail" 
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades ={
+    'Harry': "Exceeds Expectations" ,
+    'Ron': "Acceptable" ,
+    'Hermione': "Outstanding" ,
+    'Draco':  "Acceptable" ,
+    'Neville':  "Fail" 
+}
+
